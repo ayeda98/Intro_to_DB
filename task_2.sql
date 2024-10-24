bb6f657f50c568556b1fd473c08c94390955e622
Ayeda Francois DANIEL <100407568+ayeda98@users.noreply.github.com>
2024-10-24 16:48:58 +0100

    Add files via upload

--- task_2.sql
+++ task_2.sql
@@ -1,24 +1,24 @@
 USE alx_book_store;
 
-CREATE table book(book_id (Primary Key),
+CREATE table book(book_id ( PRIMARY KEY),
 title VARCHAR(130),
 author_id (Foreign Key referencing Authors table),
 price DOUBLE,
 publication_date DATE);
 
-CREATE TABLE Authors (author_id (Primary Key),
+CREATE TABLE Authors (author_id (PRIMARY KEY)),
 author_name VARCHAR(215));
 
-CREATE TABLE Customers (customer_id (Primary Key),
+CREATE TABLE Customers (customer_id (PRIMARY KEY),
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT);
 
-CREATE TABLE Orders(order_id (Primary Key),
+CREATE TABLE Orders(order_id (PRIMARY KEY),
 customer_id (Foreign Key referencing Customers table),
 order_date DATE);
 
-CREATE TABLE Order_Details (orderdetailid (Primary Key),
+CREATE TABLE Order_Details (orderdetailid (PRIMARY KEY),
 order_id (Foreign Key referencing Orders table),
 book_id (Foreign Key referencing Books table),
 quantity DOUBLE);
