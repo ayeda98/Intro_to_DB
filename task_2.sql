a1ec4975f5fc353eb11a318908cf2384dfe37f4e
Ayeda Francois DANIEL <100407568+ayeda98@users.noreply.github.com>
2024-10-24 16:59:24 +0100

    Add files via upload

--- task_2.sql
+++ task_2.sql
@@ -1,43 +1,24 @@
 USE alx_book_store;
 
--- Create the Authors table
-CREATE TABLE Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
-);
+CREATE table book(book_id ( PRIMARY KEY),
+title VARCHAR(130),
+author_id (Foreign Key referencing Authors table),
+price DOUBLE,
+publication_date DATE);
 
--- Create the Books table
-CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
-);
+CREATE TABLE Authors (author_id (PRIMARY KEY)),
+author_name VARCHAR(215));
 
--- Create the Customers table
-CREATE TABLE Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL UNIQUE,
-    address TEXT
-);
+CREATE TABLE Customers (customer_id (PRIMARY KEY),
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT);
 
--- Create the Orders table
-CREATE TABLE Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE DEFAULT CURRENT_DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-);
+CREATE TABLE Orders(order_id (PRIMARY KEY),
+customer_id (Foreign Key referencing Customers table),
+order_date DATE);
 
--- Create the Order_Details table
-CREATE TABLE Order_Details (
-    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
-);
+CREATE TABLE Order_Details (orderdetailid (PRIMARY KEY),
+order_id (Foreign Key referencing Orders table),
+book_id (Foreign Key referencing Books table),
+quantity DOUBLE);
